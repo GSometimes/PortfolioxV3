0f49a4d83e1b148745dac4e7a19f48756a7a14af
Gourav <gore.auluck@gmail.com>
2020-12-14 10:41:55 -0800

    first commit

---
+++ src/Components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css';
+import './Main.css';
+
+const Main = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Main
