3107eeebcff1c1ac2022c94171e0b61d08ca81e0
Gourav <gore.auluck@gmail.com>
2020-12-16 23:47:07 -0800

    Contact Form Update

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -37,18 +37,26 @@ const Contact = () => {
     return (
         <>
         <div id="contact" className="contactContainer">
+                <h1 id="together">Contact</h1>
                 <div className="flexContainer">
-                <h1>Let's Work Together</h1>
-                <p className="contactParagraph"></p>
-                    <div className="contentBox">
+                <p className="contactParagraph">
+                    If you made it this far I'd like to say thank you
+                     for taking the time to view my portfolio. I would love
+                     the opportunity to create something amazing with you.
+                     You can connect with me through email <a className="contactLink" href="mailto:gsometimes@outlook.com">here</a> or connect with me through 
+                     Linkedin <a className="contactLink" href="https://www.linkedin.com/in/gore-sometimes/" target="_blank" rel="noopener noreferrer">here</a>, or
+                     fill out the contact form below! Help me make the world a prettier place, one page at a time. I look forward to speaking with you. 
+                </p>
+                    
+                    {/* <div className="contentBox">
                         <p className="contact">Connect by <a className="contactLink" href="mailto:gsometimes@outlook.com">Email</a></p>
                         <p className="contact">View my <a className="contactLink" href="https://github.com/GSometimes" target="_blank" rel="noopener noreferrer">Github</a></p>
                         <p className="contact">Connect on <a className="contactLink" href="https://www.linkedin.com/in/gore-sometimes/" target="_blank" rel="noopener noreferrer">Linkedin</a></p>
-                    </div>
+                    </div> */}
 
 
-        
                     <form className="contactForm" onSubmit={handleSubmit} required>
+                    <h4 id="together">Let's Work Together!</h4>
                         <label>Name</label>
                         <input placeholder="Name"
                         value={name}
