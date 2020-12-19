fa2a57781462121fa2134f410d812d6fff4b0e66
Gourav <gore.auluck@gmail.com>
2020-12-18 22:08:08 -0800

    Updated Responsiveness

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,15 +6,15 @@ const About = () => {
         <>
             <h1 id="about">About</h1>
             <div className="aboutContainer">
-                <div className="flexContainer"> 
+                <div className="flexContainerA"> 
+                    <h3>A Father... Foodie... Gamer...</h3><br/>
                     <p className="aboutParagraph">
-                        <h2>A Father... Foodie... Gamer...</h2><br/>
                         I am a father, avid gamer, foodie and wino who's always looking to make last memories. My son, who you can see in the background, is my motivation, my “why” 
                         and the reason I do what I do. When I'm not coding you'll find me spending time with my son just hanging out, or creating something with him. You may also find me 
                         enjoying a nice glass of wine with friends or grinding my way through the Destiny universe.
                     </p>
+                    <h3>A Developer...</h3><br/>
                     <p className="aboutParagraph">
-                        <h2>A Developer...</h2><br/>
                         If you made it here then it must be for a reason and I'd like to say thank you for taking the time to view my Portfolio. 
                         From food to code, plate to page, I am a Chef turned Developer. I'm a frontend developer, specializing in React. 
                         I take simple ideas and turn them into extraordinary results. 
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -37,44 +37,43 @@ const Contact = () => {
     return (
         <>
         <div className="contactContainer">
-                <h1 id="together">Contact</h1>
-                <div className="flexContainer">
+            <h1 id="contact">Contact</h1>
+            <div className="flexContainerC">
                 <p className="contactParagraph">
                     If you made it this far I'd like to say thank you
-                     for taking the time to view my portfolio. I would love
-                     the opportunity to create something amazing with you.
-                     You can connect with me through email <a className="contactLink" href="mailto:gsometimes@outlook.com">here</a>, connect with me through 
-                     Linkedin <a className="contactLink" href="https://www.linkedin.com/in/gore-sometimes/" target="_blank" rel="noopener noreferrer">here</a>, or
-                     fill out the contact form below! Help me make the world a prettier place, one page at a time. I look forward to speaking with you. <br/><br/>
+                    for taking the time to view my portfolio. I would love
+                    the opportunity to create something amazing with you.
+                    You can connect with me through email <a className="contactLink" href="mailto:gsometimes@outlook.com">here</a>, connect with me through 
+                    Linkedin <a className="contactLink" href="https://www.linkedin.com/in/gore-sometimes/" target="_blank" rel="noopener noreferrer">here</a>, or
+                    fill out the contact form below! Help me make the world a prettier place, one page at a time. I look forward to speaking with you. <br/><br/>
                     Connect by <a className="contactLink" href="mailto:gsometimes@outlook.com">Email</a><br/>
                     View my <a className="contactLink" href="https://github.com/GSometimes" target="_blank" rel="noopener noreferrer">Github</a><br/>
                     Connect on <a className="contactLink" href="https://www.linkedin.com/in/gore-sometimes/" target="_blank" rel="noopener noreferrer">Linkedin</a><br/>
                 </p>
+                <h3 id="together">Let's Work Together!</h3>
+                <form className="contactForm" onSubmit={handleSubmit} required>
+                    <label>Name</label>
+                    <input placeholder="Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required/>
 
-                    <form className="contactForm" onSubmit={handleSubmit} required>
-                    <h3 id="together">Let's Work Together!</h3>
-                        <label>Name</label>
-                        <input placeholder="Name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        required/>
+                    <label>Email</label>
+                    <input placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required/>
 
-                        <label>Email</label>
-                        <input placeholder="Email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required/>
+                    <label>Message</label>
+                    <textarea placeholder="Message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    required></textarea>
+                    
+                    <button type="submit" style={{background: loader ? "#ccc" : "#71A9F7" , height:"30px"}}>Submit</button>
+                </form>
 
-                        <label>Message</label>
-                        <textarea placeholder="Message"
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                        required></textarea>
-                        
-                        <button type="submit" style={{background: loader ? "#ccc" : "#71A9F7" , height:"30px"}}>Submit</button>
-                    </form>
-
-                </div>
+            </div>
         </div>
 
         </>
